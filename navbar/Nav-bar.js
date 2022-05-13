45e176d812e10c40d9429b8b277c09ba47ce2ae5
c.sipiran07@gmail.com <carlos0718>
2022-05-12 23:07:27 -0300

    href modificado a commerceJS

--- navbar/Nav-bar.js
+++ navbar/Nav-bar.js
@@ -34,11 +34,11 @@ const style = {
 
 const NavBar = `<nav style='${style.nav}'>
                     <ul style='${style.ul}'>
-                        <li  id="title" style="${style.title}"><a style="${style.a}" href="../"> Clothing Store </a></li>
-                        <li style="${style.li}"><a style="${style.a}" href="../">Home</a></li>
-                        <li style="${style.li}"><a style="${style.a}" href="../">About us</a></li>
-                        <li style="${style.li}"><a style="${style.a}" href="../">Contact</a></li>
-                        <li style="${style.li}"><img id="hover-carrito" src="../images/carrito-de-compras.png" alt="carrito" width=35px />
+                        <li  id="title" style="${style.title}"><a style="${style.a}" href="/commerceJS"> Clothing Store </a></li>
+                        <li style="${style.li}"><a style="${style.a}" href="commerceJS">Home</a></li>
+                        <li style="${style.li}"><a style="${style.a}" href="commerceJS">About us</a></li>
+                        <li style="${style.li}"><a style="${style.a}" href="commerceJS">Contact</a></li>
+                        <li style="${style.li}"><img id="hover-carrito" src="images/carrito-de-compras.png" alt="carrito" width=35px />
                             <span id="icon-cart" style="${style.tooltip}">
                                 0    
                             </span>
