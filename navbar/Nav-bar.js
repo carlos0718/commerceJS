bc2effc8f790fdc8bf1246aa7814e9f626c97745
c.sipiran07@gmail.com <carlos0718>
2022-05-12 23:46:43 -0300

    fixed img

--- navbar/Nav-bar.js
+++ navbar/Nav-bar.js
@@ -34,15 +34,16 @@ const style = {
 
 const NavBar = `<nav style='${style.nav}'>
                     <ul style='${style.ul}'>
-                        <li  id="title" style="${style.title}"><a style="${
-	style.a
-}" href="/commerceJS"> Clothing Store </a></li>
-                        <li style="${style.li}"><a style="${style.a}" href="commerceJS">Home</a></li>
+                        <li  id="title" style="${style.title}">
+                            <a style="${style.a}" href="/commerceJS"> Clothing Store </a>
+                        </li>
+                        <li style="${style.li}"><a style="${style.a}" >Home</a></li>
                         <li style="${style.li}"><a style="${style.a}" >About us</a></li>
                         <li style="${style.li}"><a style="${style.a}" >Contact</a></li>
-                        <li style="${style.li}"><img id="hover-carrito" src="${
-	"images/carrito-de-compras.png" || "/images/carrito-de-compras.png"
-}" alt="carrito" width=35px />
+                        <li style="${style.li}"><img id="hover-carrito" 
+                            src="${
+								"images/carrito-de-compras.png" || "../images/carrito-de-compras.png"
+							}" alt="carrito" width=35px />
                             <span id="icon-cart" style="${style.tooltip}">
                                 0    
                             </span>
